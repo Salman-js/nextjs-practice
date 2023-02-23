40994346fff73e8dcd8d1967f77eea1865244689
Salman M <salmanmoh.eth@gmail.com>
2023-02-23 10:09:42 +0300

    getStaticProps

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+function NoPageFound() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <p className='text-4xl font-bold'>404</p>
+    </div>
+  );
+}
+
+export default NoPageFound;
